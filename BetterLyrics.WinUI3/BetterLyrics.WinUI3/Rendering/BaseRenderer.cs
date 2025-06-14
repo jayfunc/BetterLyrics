fdcc51d5f250d8b38427063277cbc5b2e1b09d6a
Zhe Fang <founchoo@outlook.com>
2025-06-14 15:56:20 -0400

    fix: dynamic background on desktop lyrics not working after re-open it

---
+++ BetterLyrics.WinUI3/BetterLyrics.WinUI3/Rendering/BaseRenderer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BetterLyrics.WinUI3.Rendering
+{
+    public class BaseRenderer
+    {
+        public TimeSpan TotalTime { get; set; } = TimeSpan.Zero;
+        public TimeSpan ElapsedTime { get; set; } = TimeSpan.Zero;
+    }
+}
