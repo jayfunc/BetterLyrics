1d0c63b25a3096fa0449cfed0596e617d0411e34
Zhe Fang <founchoo@outlook.com>
2025-07-26 07:51:27 -0400

    fix #63

--- BetterLyrics.WinUI3/BetterLyrics.WinUI3/App.xaml.cs
+++ BetterLyrics.WinUI3/BetterLyrics.WinUI3/App.xaml.cs
@@ -50,10 +50,10 @@ namespace BetterLyrics.WinUI3
 
             _logger = Ioc.Default.GetRequiredService<ILogger<App>>();
 
-            UnhandledException += App_UnhandledException;
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-            AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
-            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            //UnhandledException += App_UnhandledException;
+            //AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            //AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+            //TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
         }
 
         protected override void OnLaunched(LaunchActivatedEventArgs args)
