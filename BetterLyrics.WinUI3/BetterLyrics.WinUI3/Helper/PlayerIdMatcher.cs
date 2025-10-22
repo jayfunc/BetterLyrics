2ccda2e2f7a9d072338d3a8b4666fb2760bd3829
Zhe Fang <founchoo@outlook.com>
2025-10-22 09:24:16 -0400

    fix: improve docked mode window sizeing behaviors

--- BetterLyrics.WinUI3/BetterLyrics.WinUI3/Helper/PlayerIdMatcher.cs
+++ BetterLyrics.WinUI3/BetterLyrics.WinUI3/Helper/PlayerIdMatcher.cs
@@ -5,7 +5,7 @@ namespace BetterLyrics.WinUI3.Helper
 {
     public static class PlayerIdMatcher
     {
-        private static readonly List<string> neteaseFamilyRegex =
+        private static readonly List<string> _neteaseFamilyRegex =
         [
             "cloudmusic.exe", //NetEaseCloudMusic
             "^17588BrandonWong\\.LyricEase_", //LyricEase
@@ -14,7 +14,7 @@ namespace BetterLyrics.WinUI3.Helper
 
         public static bool IsNeteaseFamily(string player)
         {
-            foreach (var regex in neteaseFamilyRegex)
+            foreach (var regex in _neteaseFamilyRegex)
             {
                 var isMatch = Regex.IsMatch(player, regex);
                 if (isMatch) return true;
