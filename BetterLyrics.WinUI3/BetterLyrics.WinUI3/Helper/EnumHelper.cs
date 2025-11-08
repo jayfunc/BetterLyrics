1eb1d2d72b366532c492d8529d9e9a3839da4921
Zhe Fang <founchoo@outlook.com>
2025-11-08 11:41:59 -0500

    feat: track stop; chores: ui/ux adjustments, bump version code.

---
+++ BetterLyrics.WinUI3/BetterLyrics.WinUI3/Helper/EnumHelper.cs
@@ -0,0 +1,21 @@
+﻿using BetterLyrics.WinUI3.Enums;
+using BetterLyrics.WinUI3.Models.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterLyrics.WinUI3.Helper
+{
+    public static class EnumExtensions
+    {
+        public static T GetNext<T>(this T value) where T : struct, Enum
+        {
+            T[] values = Enum.GetValues<T>();
+            int currentIndex = Array.IndexOf(values, value);
+            int nextIndex = (currentIndex + 1) % values.Length;
+            return values[nextIndex];
+        }
+    }
+}
