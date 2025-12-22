81296996c1258f88d5f93ecdb14f49e88ed30fb0
Zhe Fang <founchoo@outlook.com>
2025-12-22 16:45:41 -0500

    fix: song info fade

--- BetterLyrics.WinUI3/BetterLyrics.WinUI3/Converter/FileSourceTypeToIconConverter.cs
+++ BetterLyrics.WinUI3/BetterLyrics.WinUI3/Converter/FileSourceTypeToIconConverter.cs
@@ -13,14 +13,14 @@ namespace BetterLyrics.WinUI3.Converter
             {
                 return type switch
                 {
-                    FileSourceType.Local => new FontIcon { Glyph = "\uE8B7" }, // Folder
-                    FileSourceType.SMB => new FontIcon { Glyph = "\uE839" },   // Network
-                    FileSourceType.FTP => new FontIcon { Glyph = "\uE838" },   // Globe
-                    FileSourceType.WebDav => new FontIcon { Glyph = "\uE753" }, // Cloud
-                    _ => new FontIcon { Glyph = "\uE8B7" }
+                    FileSourceType.Local => "\uE8B7", // Folder
+                    FileSourceType.SMB => "\uE839",   // Network
+                    FileSourceType.FTP => "\uE838",   // Globe
+                    FileSourceType.WebDav => "\uE753", // Cloud
+                    _ => "\uE8B7"
                 };
             }
-            return new FontIcon { Glyph = "\uE8B7" };
+            return "\uE8B7";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
