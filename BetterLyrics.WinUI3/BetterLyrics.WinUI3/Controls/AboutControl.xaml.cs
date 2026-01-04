18ce6d3a578532edc24c0ce4d814fdb80698d6ef
Zhe Fang <founchoo@outlook.com>
2026-01-04 11:39:47 -0500

    chores: improve thanks list

--- BetterLyrics.WinUI3/BetterLyrics.WinUI3/Controls/AboutControl.xaml.cs
+++ BetterLyrics.WinUI3/BetterLyrics.WinUI3/Controls/AboutControl.xaml.cs
@@ -11,54 +11,12 @@ namespace BetterLyrics.WinUI3.Controls
 {
     public sealed partial class AboutControl : UserControl
     {
-        private bool _isCreditsScrolling = false;
         public AboutControlViewModel ViewModel => (AboutControlViewModel)DataContext;
 
         public AboutControl()
         {
             InitializeComponent();
             DataContext = Ioc.Default.GetRequiredService<AboutControlViewModel>();
-        }
-
-        private async void Patron_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-        {
-            CompositionTarget.Rendering += CompositionTarget_Rendering;
-            CreditsReel.Visibility = Microsoft.UI.Xaml.Visibility.Visible;
-            CreditsReel.Opacity = 1;
-            _isCreditsScrolling = true;
-        }
-
-        private void CompositionTarget_Rendering(object? sender, object e)
-        {
-            if (_isCreditsScrolling)
-            {
-                CreditsReelScrollViewer.ChangeView(null, CreditsReelScrollViewer.VerticalOffset + 0.5, null);
-            }
-        }
-
-        private async void CreditsReel_Tapped(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
-        {
-            CreditsReel.Opacity = 0;
-            await Task.Delay(Constants.Time.AnimationDuration);
-            CreditsReel.Visibility = Microsoft.UI.Xaml.Visibility.Collapsed;
-            CompositionTarget.Rendering -= CompositionTarget_Rendering;
-            CreditsReelScrollViewer.ChangeView(null, 0, null);
-        }
-
-        private void CreditsReel_SizeChanged(object sender, Microsoft.UI.Xaml.SizeChangedEventArgs e)
-        {
-            CreditsReelHeader.LineHeight = e.NewSize.Height;
-            CreditsReelFooter.LineHeight = e.NewSize.Height / 2;
-        }
-
-        private void RichTextBlock_PointerEntered(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
-        {
-            _isCreditsScrolling = false;
-        }
-
-        private void RichTextBlock_PointerExited(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
-        {
-            _isCreditsScrolling = true;
         }
 
         private void WeChat_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
