26d85a220feffb9a7c6b896a0e967de4a8573532
Zhe Fang <founchoo@outlook.com>
2026-01-10 10:06:11 -0500

    fix: lyrics parser (line endtime)

--- BetterLyrics.WinUI3/BetterLyrics.WinUI3/Parsers/LyricsParser/LyricsParser.QrcKrc.cs
+++ BetterLyrics.WinUI3/BetterLyrics.WinUI3/Parsers/LyricsParser/LyricsParser.QrcKrc.cs
@@ -21,7 +21,6 @@ namespace BetterLyrics.WinUI3.Parsers.LyricsParser
                     var lineWrite = new LyricsLine
                     {
                         StartMs = lineRead.StartTime ?? 0,
-                        EndMs = lineRead.EndTime,
                         PrimaryText = lineRead.Text,
                         PrimarySyllables = [],
                     };
