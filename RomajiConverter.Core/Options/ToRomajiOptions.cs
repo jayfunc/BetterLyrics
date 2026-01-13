e91abe6c7e8ee2081ad4974d393e0471c1d294af
Zhe Fang <founchoo@outlook.com>
2026-01-13 17:23:52 -0500

    feat: add auto zip process after successfully building plugins

--- RomajiConverter.Core/Options/ToRomajiOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RomajiConverter.Core.Options
-{
-    public class ToRomajiOptions
-    {
-        public float ChineseRate { get; set; } = 1f;
-
-        public bool IsParticleAsPronunciation { get; set; } = true;
-    }
-}
