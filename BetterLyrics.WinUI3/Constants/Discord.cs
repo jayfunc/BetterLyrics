7ca12befe3040a45a6eb9640f2657496b76da8b7
Zhe Fang <founchoo@outlook.com>
2026-01-13 17:23:52 -0500

    feat: add auto zip process after successfully building plugins

---
+++ BetterLyrics.WinUI3/Constants/Discord.cs
@@ -0,0 +1,7 @@
+﻿namespace BetterLyrics.WinUI3.Constants
+{
+    public class Discord
+    {
+        public const string AppID = "1437570788264906947";
+    }
+}
---
+++ BetterLyrics.WinUI3/Constants/LastFM.cs
@@ -0,0 +1,10 @@
+﻿namespace BetterLyrics.WinUI3.Constants
+{
+    public static class LastFM
+    {
+        public const string ApiKey = "0ddc6bf987ca85d18424340c993938cc";
+        public const string SharedSecret = "dc14a1c74764aed5a2aa38e85c7094c0";
+        public const string UnAuthUrl = "https://www.last.fm/settings/applications";
+        public const string SessionKeyCredentialKey = "LastFMSessionKey";
+    }
+}
