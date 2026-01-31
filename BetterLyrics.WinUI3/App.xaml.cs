096088fd3fcdf74d9ed73b5853bad9747b91ae4d
Zhe Fang <founchoo@outlook.com>
2026-01-31 13:53:40 -0500

    revert: rollback to split project mode for better building; fix: font exception

--- BetterLyrics.WinUI3/App.xaml.cs
+++ BetterLyrics.WinUI3/App.xaml.cs
@@ -40,7 +40,8 @@ namespace BetterLyrics.WinUI3
         protected override async void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             var splashWindow = WindowHook.OpenOrShowWindow<SplashWindow>();
-            
+
+            await Task.Delay(100);
             await InitAppServicesAsync();
             
             HandleNormalLaunch();
