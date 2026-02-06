62cff3566acf5f0d42173bb274557d944170d08e
Zhe Fang <founchoo@outlook.com>
2026-02-05 20:30:50 -0500

    feat: real-time file sync is back

---
+++ BetterLyrics.WinUI3/Services/FileWatchService/IFileWatchService.cs
@@ -0,0 +1,12 @@
+﻿using BetterLyrics.WinUI3.Events;
+using BetterLyrics.WinUI3.Models.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetterLyrics.WinUI3.Services.FileWatchService
+{
+    public interface IFileWatchService
+    {
+    }
+}
