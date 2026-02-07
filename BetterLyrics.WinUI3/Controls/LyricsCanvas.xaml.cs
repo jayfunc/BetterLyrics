6897d9819602abd64f740de43dfa9047247c9098
Zhe Fang <founchoo@outlook.com>
2026-02-07 16:55:36 -0500

    feat: modern debug layer

--- BetterLyrics.WinUI3/Controls/LyricsCanvas.xaml.cs
+++ BetterLyrics.WinUI3/Controls/LyricsCanvas.xaml.cs
@@ -21,6 +21,7 @@ using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Storage.Streams;
@@ -419,21 +420,50 @@ namespace BetterLyrics.WinUI3.Controls
                 fgColor: _albumArtThemeColors.FgFontColor,
                 currentProgressMs: _songPositionWithOffset.TotalMilliseconds);
 
-#if DEBUG && false
-            args.DrawingSession.DrawText(
-                    $"Lyrics render start pos: ({(int)_renderLyricsStartX}, {(int)_renderLyricsStartY})\n" +
-                    $"Lyrics render size: [{(int)_renderLyricsWidth} x {(int)_renderLyricsHeight}]\n" +
-                    $"Lyrics actual height: {LyricsLayoutManager.CalculateActualHeight(_renderLyricsLines)}\n" +
-                    $"Playing line (idx): {_primaryPlayingLineIndex}\n" +
-                    $"Mouse hovering line (idx): {_mouseHoverLineIndex}\n" +
-                    $"Visible lines range (idx): [{_visibleRange.Start}, {_visibleRange.End}]\n" +
-                    $"Total line count: {LyricsLayoutManager.CalculateMaxRange(_renderLyricsLines).End + 1}\n" +
-                    $"Played: {_songPosition} / {TimeSpan.FromMilliseconds(_gsmtcService.CurrentSongInfo.DurationMs)}\n" +
-                    $"Y offset: {_canvasYScrollTransition.Value}\n" +
-                    $"User scroll offset: {_mouseYScrollTransition.Value}",
-                new Vector2(0, 0), Colors.Red);
-#endif
+#if DEBUG
+            string debugText =
+                $"FPS          : {(1.0 / args.Timing.ElapsedTime.TotalSeconds):00.0} (Avg: {args.Timing.UpdateCount / args.Timing.TotalTime.TotalSeconds:00.0})\n" +
+                $"----------------------------------------\n" +
+                $"Render Pos   : [{(int)_renderLyricsStartX}, {(int)_renderLyricsStartY}]\n" +
+                $"Render Size  : [{(int)_renderLyricsWidth} x {(int)_renderLyricsHeight}]\n" +
+                $"Actual Height: {LyricsLayoutManager.CalculateActualHeight(_renderLyricsLines)} px\n" +
+                $"----------------------------------------\n" +
+                $"Playing Line : #{_primaryPlayingLineIndex}\n" +
+                $"Hover Line   : #{_mouseHoverLineIndex}\n" +
+                $"Visible Range: [{_visibleRange.Start} -> {_visibleRange.End}]\n" +
+                $"Total Lines  : {LyricsLayoutManager.CalculateMaxRange(_renderLyricsLines).End + 1}\n" +
+                $"----------------------------------------\n" +
+                $"Time         : {_songPosition:mm\\:ss} / {TimeSpan.FromMilliseconds(_gsmtcService.CurrentSongInfo.DurationMs):mm\\:ss}\n" +
+                $"Y Offset     : {_canvasYScrollTransition.Value:0.00}\n" +
+                $"User Scroll  : {_mouseYScrollTransition.Value:0.00}";
 
+            using (var format = new Microsoft.Graphics.Canvas.Text.CanvasTextFormat
+            {
+                FontFamily = "Consolas",
+                FontSize = 13,
+                VerticalAlignment = Microsoft.Graphics.Canvas.Text.CanvasVerticalAlignment.Top,
+                HorizontalAlignment = Microsoft.Graphics.Canvas.Text.CanvasHorizontalAlignment.Left
+            })
+            using (var layout = new Microsoft.Graphics.Canvas.Text.CanvasTextLayout(args.DrawingSession, debugText, format, 2000f, 2000f))
+            {
+                var textBounds = layout.LayoutBounds;
+                float padding = 12f;
+                float margin = 12f;
+
+                float boxWidth = (float)textBounds.Width + (padding * 2);
+                float boxHeight = (float)textBounds.Height + (padding * 2);
+                float canvasWidth = (float)sender.Size.Width;
+
+                float xPos = canvasWidth - boxWidth - margin;
+                float yPos = margin;
+
+                var bgRect = new Rect(xPos, yPos, boxWidth, boxHeight);
+
+                args.DrawingSession.FillRectangle(bgRect, Color.FromArgb(128, 10, 10, 10));
+                args.DrawingSession.DrawRectangle(bgRect, Colors.Cyan, 1.0f);
+                args.DrawingSession.DrawTextLayout(layout, new Vector2(xPos + padding, yPos + padding), Colors.GreenYellow);
+            }
+#endif
         }
 
         private void Canvas_Update(ICanvasAnimatedControl sender, CanvasAnimatedUpdateEventArgs args)
