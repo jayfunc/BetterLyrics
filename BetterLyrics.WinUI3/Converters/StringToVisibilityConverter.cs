54daafd6ac8d5029444bc4e0cc206c805b3c43ae
Zhe Fang <founchoo@outlook.com>
2026-02-17 20:35:30 -0500

    fix: lyrics card  style issue

---
+++ BetterLyrics.WinUI3/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetterLyrics.WinUI3.Converters
+{
+    public partial class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (string.IsNullOrEmpty(value as string))
+            {
+                return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
