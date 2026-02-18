3232439de449ea4751927239dfbd1bd2f8337de0
Zhe Fang <founchoo@outlook.com>
2026-02-18 14:24:58 -0500

    fix: qq lyrics source parse

--- BetterLyrics.WinUI3/Services/TransliterationService/TransliterationService.cs
+++ BetterLyrics.WinUI3/Services/TransliterationService/TransliterationService.cs
@@ -28,10 +28,6 @@ namespace BetterLyrics.WinUI3.Services.TransliterationService
         public async Task<(string, TransliterationSearchProvider)> TransliterateText(string text, string targetLangCode, CancellationToken token)
         {
             string? result = null;
-            if (string.IsNullOrWhiteSpace(text))
-            {
-                throw new Exception(text + " is empty or null.");
-            }
 
             var pluginInfo = _settingsService.AppSettings.PluginsInfo.FirstOrDefault(x => x.Plugin is ILyricsTransliterator);
             if (pluginInfo != null)
@@ -39,7 +35,8 @@ namespace BetterLyrics.WinUI3.Services.TransliterationService
                 var plugin = (ILyricsTransliterator?)pluginInfo.Plugin;
                 if (plugin != null)
                 {
-                    result = await plugin.GetTransliterationAsync(text, targetLangCode);
+                    result = await plugin.GetTransliterationAsync(text, targetLangCode, token);
+                    token.ThrowIfCancellationRequested();
                 }
             }
 
