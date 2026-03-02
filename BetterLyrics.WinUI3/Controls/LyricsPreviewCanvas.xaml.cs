d8aecd9f443e4c76f3d041a091d2159888e32e4f
Zhe Fang <founchoo@outlook.com>
2026-03-02 16:36:16 -0500

    chore

---
+++ BetterLyrics.WinUI3/Controls/LyricsPreviewCanvas.xaml.cs
@@ -0,0 +1,35 @@
+using BetterLyrics.WinUI3.Renderer;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace BetterLyrics.WinUI3.Controls
+{
+    public sealed partial class LyricsPreviewCanvas : UserControl
+    {
+        private readonly LyricsRenderer _lyricsRenderer = new();
+
+        public LyricsPreviewCanvas()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
+        {
+        }
+    }
+}
