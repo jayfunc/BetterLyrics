c01ca064639541624cf5a3a2eef74387bec3d5c6
Zhe Fang <founchoo@outlook.com>
2026-03-07 14:14:38 -0500

    feat: add more animated control styles

---
+++ BetterLyrics.WinUI3/Converters/IntNegationToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetterLyrics.WinUI3.Converters
+{
+    public partial class IntNegationToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value is int intVal && intVal != 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
