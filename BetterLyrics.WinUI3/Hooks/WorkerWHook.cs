62b95564ee02e1b746c42e415604c6b66d529c90
Zhe Fang <founchoo@outlook.com>
2026-03-14 08:41:09 -0400

    fix: WorkW

--- BetterLyrics.WinUI3/Hooks/WorkerWHook.cs
+++ BetterLyrics.WinUI3/Hooks/WorkerWHook.cs
@@ -20,34 +20,20 @@ namespace BetterLyrics.WinUI3.Hooks
 
             IntPtr windowHandle = WindowNative.GetWindowHandle(window);
 
-            HWND progman = User32.FindWindow("Progman", null);
-            
+            HWND hProgman = User32.FindWindow("Progman", null);
+
             var _ = IntPtr.Zero;
             // 发送 0x052C 消息，触发 WorkerW 层的生成
-            User32.SendMessageTimeout(progman, 0x052C, IntPtr.Zero, IntPtr.Zero, 0, 1000, ref _);
+            User32.SendMessageTimeout(hProgman, 0x052C, IntPtr.Zero, IntPtr.Zero, 0, 1000, ref _);
 
-            HWND workerW = HWND.NULL;
-
-            // 遍历寻找负责壁纸的 WorkerW
-            User32.EnumWindows((tophandle, topparamhandle) =>
-            {
-                HWND p = User32.FindWindowEx(tophandle, HWND.NULL, "SHELLDLL_DefView", null);
-                if (p != IntPtr.Zero)
-                {
-                    workerW = User32.FindWindowEx(IntPtr.Zero, tophandle, "WorkerW", null);
-                }
-                return true;
-            }, IntPtr.Zero);
+            HWND hWorkerW = User32.FindWindowEx(hProgman, HWND.NULL, "WorkerW", null);
 
             // 设置父窗口
-            if (workerW != HWND.NULL)
+            if (hWorkerW != HWND.NULL)
             {
-                User32.SetParent(windowHandle, workerW);
+                User32.SetParent(windowHandle, hWorkerW);
             }
-            else
-            {
-                User32.SetParent(windowHandle, progman);
-            }
+
         }
 
         public static void UnpinFromDesktop(Window window)
@@ -56,7 +42,7 @@ namespace BetterLyrics.WinUI3.Hooks
 
             IntPtr windowHandle = WindowNative.GetWindowHandle(window);
 
-            User32.SetParent(windowHandle, IntPtr.Zero);
+            User32.SetParent(windowHandle, HWND.NULL);
         }
 
     }
