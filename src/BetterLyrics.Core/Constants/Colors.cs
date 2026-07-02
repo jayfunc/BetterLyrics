fb6fbf92c687b66d67bb8d4fd8ce2f3f720e02eb
jayfunc <founchoo@outlook.com>
2026-07-02 10:57:33 +0800

    fix: transparent define

--- src/BetterLyrics.Core/Constants/Colors.cs
+++ src/BetterLyrics.Core/Constants/Colors.cs
@@ -6,7 +6,7 @@ namespace BetterLyrics.Core.Constants
     {
         public static readonly AppColor White = new(255, 255, 255, 255);
         public static readonly AppColor Black = new(255, 0, 0, 0);
-        public static readonly AppColor Transparent = new(255, 255, 255, 255);
+        public static readonly AppColor Transparent = new(0, 0, 0, 0);
         public static readonly AppColor Cyan = new(255, 255, 0, 255);
         public static readonly AppColor GreenYellow = new(255, 255, 0, 255);
     }
